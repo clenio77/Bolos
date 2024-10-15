1b28b1695739893f51edfd688bff590d6ce47281
clenio <mouraaf@hotmail.com>
2024-10-15 19:13:59 -0300

    add sqlalquemy ao requirement

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, text
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, joinedload
 
 # Configuração do banco de dados
